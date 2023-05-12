fc0e2e027f8de492a5faf759bb3c1abe37ad8884
danielpredel <danielpreciadodelgadillo@gmail.com>
2023-05-11 21:07:55 -0600

    Ordenacion de citas registradas de la mas proxima a la mas lejana

--- src/app/reservaciones/citas-reservadas/citas-reservadas.component.ts
+++ src/app/reservaciones/citas-reservadas/citas-reservadas.component.ts
@@ -1,20 +1,51 @@
-import { Component, OnInit } from '@angular/core';
-import { Cita } from '../cita.model';
-import { AlmacenamientoCitasService } from '../almacenamiento-citas.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  Cita
+} from '../cita.model';
+import {
+  AlmacenamientoCitasService
+} from '../almacenamiento-citas.service';
 
 @Component({
   selector: 'app-citas-reservadas',
   templateUrl: './citas-reservadas.component.html',
   styleUrls: ['./citas-reservadas.component.css']
 })
-export class CitasReservadasComponent  implements OnInit {
+export class CitasReservadasComponent implements OnInit {
   citas: Cita[] = [];
 
   constructor(private citasService: AlmacenamientoCitasService) {}
 
   ngOnInit() {
     this.citas = this.citasService.getCitas();
-    // console.log(this.citas);
+    this.citas.sort((a, b) => {
+      if (a.year < b.year) {
+        return -1;
+      } else if (a.year == b.year) {
+        if (a.mes < b.mes) {
+          return -1;
+        } else if (a.mes == b.mes) {
+          if (a.dia < b.dia) {
+            return -1;
+          } else if (a.dia == b.dia) {
+            if (a.hora < b.hora) {
+              return -1;
+            } else {
+              return 1;
+            }
+          } else {
+            return 1;
+          }
+        } else {
+          return 1;
+        }
+      } else {
+        return 1;
+      }
+    });
   }
 
 }
