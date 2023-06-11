a7de093fcd9ff076a3ddbdcb80824d67b39990dd
danielpredel <danielpreciadodelgadillo@gmail.com>
2023-06-11 03:51:14 -0600

    Agregar doctores a la API

--- backend/routes/doctores.js
+++ backend/routes/doctores.js
@@ -75,6 +75,95 @@ router.get('/', function (req, res, next) {
             edad: 49,
             sexo: 'Masculino',
             foto: 'https://elcomercio.pe/resizer/02f9Q-CCurV2c8vK5cHxX0Rky84=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/GPWVKK5ADJF2ZKYJN3Y3WJKOQQ.jpg'
+        },
+        {
+            nombre: 'Emiliano',
+            apellido: 'Navarro',
+            especialidad: 'Medicina Familiar y Comunitaria',
+            correo: 'emilionavarro@gmail.com',
+            edad: 49,
+            sexo: 'Masculino',
+            foto: 'https://media.istockphoto.com/id/1203995945/es/foto/retrato-de-hombre-maduro-doctor-que-lleva-abrigo-blanco-de-pie-en-el-pasillo-del-hospital.jpg?s=612x612&w=0&k=20&c=TLFMEYbqmt5noGITcY6PRgaX4MfmfE2TYXehgiCDDuk='},
+        {
+            nombre: 'Santiago',
+            apellido: 'Morales',
+            especialidad: 'Anestesiología',
+            correo: 'santiagomorales@gmail.com',
+            edad: 35,
+            sexo: 'Masculino',
+            foto: 'https://img.freepik.com/foto-gratis/doctor-brazos-cruzados-sobre-fondo-blanco_1368-5790.jpg?w=2000'
+        },
+        {
+            nombre: 'Leonardo',
+            apellido: 'Fuentes',
+            especialidad: 'Infectología',
+            correo: 'leofuentes@gmail.com',
+            edad: 49,
+            sexo: 'Masculino',
+            foto: 'https://elcomercio.pe/resizer/02f9Q-CCurV2c8vK5cHxX0Rky84=/1200x900/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/GPWVKK5ADJF2ZKYJN3Y3WJKOQQ.jpg'
+        },
+        {
+            nombre: 'Gabriel',
+            apellido: 'Mendoza',
+            especialidad: 'Otorrinolaringología',
+            correo: 'gabrielmendoza@gmail.com',
+            edad: 66,
+            sexo: 'Masculino',
+            foto: 'https://cdn.euroinnova.edu.es/img/subidasEditor/doctor-5871743_640-1610073541.webp'
+        },
+        {
+            nombre: 'Adrián',
+            apellido: 'Cervantes',
+            especialidad: 'Urología',
+            correo: 'adriancervantes@gmail.com',
+            edad: 31,
+            sexo: 'Masculino',
+            foto: 'https://i.pinimg.com/originals/ce/63/47/ce63475562f960ede351c2533415adc5.jpg'
+        },
+        {
+            nombre: 'Valentina',
+            apellido: 'Castillo',
+            especialidad: 'Ginecología',
+            correo: 'valentinacastillo@gmail.com',
+            edad: 67,
+            sexo: 'Femenino',
+            foto: 'https://www.ecestaticos.com/file/d02137e2afb167582c22fb98077f2265/1630407178-2-pilar-tornos.jpg'
+        },
+        {
+            nombre: 'Camila',
+            apellido: 'Espinoza',
+            especialidad: 'Nefrología',
+            correo: 'camiespinoza@gmail.com',
+            edad: 32,
+            sexo: 'Femenino',
+            foto: 'https://st4.depositphotos.com/1188954/20017/i/600/depositphotos_200171296-stock-photo-pretty-female-doctor-shot-in.jpg'
+        },
+        {
+            nombre: 'Fernanda',
+            apellido: 'Guzmán',
+            especialidad: 'Dermatología',
+            correo: 'ferguzman@gmail.com',
+            edad: 33,
+            sexo: 'Femenino',
+            foto: 'https://img.freepik.com/foto-gratis/hermosa-joven-doctora-mirando-camara-oficina_1301-7807.jpg?w=2000'
+        },
+        {
+            nombre: 'Isabella',
+            apellido: 'Rosales',
+            especialidad: 'Cardiología',
+            correo: 'isarosales@gmail.com',
+            edad: 27,
+            sexo: 'Femenino',
+            foto: 'https://www.eluniversal.com.mx/resizer/4eIUHOAZfto7OWaeXCRUiRlexCE=/1100x666/cloudfront-us-east-1.images.arcpublishing.com/eluniversal/7NZOYSQDIZHQNNKQGQ4GEXDNF4.jpg'
+        },
+        {
+            nombre: 'Daniela',
+            apellido: 'Torres',
+            especialidad: 'Pediatría',
+            correo: 'danitorres@gmail.com',
+            edad: 31,
+            sexo: 'Femenino',
+            foto: 'https://st3.depositphotos.com/4678277/13419/i/600/depositphotos_134194196-stock-photo-portrait-of-pretty-female-doctor.jpg'
         }
     ]);
 });
