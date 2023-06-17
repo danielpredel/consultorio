a054ae7e1467a1eac86a02870db40e5074ce474f
danielpredel <danielpreciadodelgadillo@gmail.com>
2023-06-17 15:38:56 -0600

    Borrar Link de Citas del Footer

--- src/app/medicos.service.ts
+++ src/app/medicos.service.ts
@@ -14,8 +14,7 @@ export class MedicosService {
   constructor(private http: HttpClient) {}
 
   retornarDatos(){
-    // return this.http.get("https://consultorio.fly.dev/api/doctores");
-    return this.http.get("http://localhost:3000/api/doctores");
+    return this.http.get("https://consultorio.fly.dev/api/doctores");
   }
 
   
