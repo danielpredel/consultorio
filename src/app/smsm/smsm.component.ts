b581b5f985e63f8a1a7ec3b1c25ebee084aca1f2
josueDanielpp <parradavalosj@gmail.com>
2023-06-16 19:58:46 -0600

    validaciones

--- src/app/smsm/smsm.component.ts
+++ src/app/smsm/smsm.component.ts
@@ -16,6 +16,8 @@ import {
 import {
   LoginServiceService
 } from '../login-service.service';
+import { FirebaseReservaService } from '../reservaciones/firebase-reserva.service';
+import { Usuarios } from '../reservaciones/usuario.model';
 
 @Component({
   selector: 'app-smsm',
@@ -27,8 +29,9 @@ export class SMSMComponent {
   token: string = "";
   enviado: boolean = false;
   cat: boolean = false;
+  usuario:Usuarios | undefined;
   //confirmationResult:any;
-  constructor(private router: Router, private sms: LoginServiceService) {
+  constructor(private router: Router, private sms: LoginServiceService,private fire:FirebaseReservaService) {
     //this.recapcha();
   }
 
@@ -41,28 +44,48 @@ export class SMSMComponent {
   }
 
   enviarCodigo(tele: string) {
-    const phoneNumber = tele;
-    console.log(tele);
-    const appVerifier = this.recaptcha.response;
-    /// response.response;
-    const auth = getAuth();
-    console.log(this.recaptcha);
-    signInWithPhoneNumber(auth, phoneNumber, this.recaptcha)
-      .then((confirmationResult) => {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        //window.confirmationResult = confirmationResult;
-        console.log("holaa :)");
-        console.log("entraste ;) avr ")
-        this.confirmationResult = confirmationResult;
-        console.log(this.confirmationResult)
-        this.enviado = true;
-        // ...
-      }).catch((error) => {
-        // Error; SMS not sent
-        console.log(error);
-        // ...
-      });
+    console.log("numero: "+tele.substring(3))
+    this.fire.buscarnum(tele.substring(3)).subscribe(usu=>{
+      var hola="";
+      hola= Object.keys(usu)[0];
+     if(hola!=undefined){
+      console.log("cadena "+hola);
+      console.log("veamos: "+this.sms.usuariolog);
+        this.sms.usuariolog=hola;
+        this.fire.vernom(hola).subscribe(nombre=>{
+          this.sms.nombre=JSON.stringify(nombre);
+          const phoneNumber = tele;
+          console.log(tele);
+          const appVerifier = this.recaptcha.response;
+          /// response.response;
+          const auth = getAuth();
+          console.log(this.recaptcha);
+          signInWithPhoneNumber(auth, phoneNumber, this.recaptcha)
+            .then((confirmationResult) => {
+              // SMS sent. Prompt user to type the code from the message, then sign the
+              // user in with confirmationResult.confirm(code).
+              //window.confirmationResult = confirmationResult;
+              console.log("holaa :)");
+              console.log("entraste ;) avr ")
+              this.confirmationResult = confirmationResult;
+              console.log(this.confirmationResult)
+              this.enviado = true;
+              // ...
+            }).catch((error) => {
+              // Error; SMS not sent
+              console.log(error);
+              // ...
+            });
+        });
+     }else{
+      swal("No esta Registrado este numero" , "registrate para poder ingresar", "error");
+     }
+    })
+    
+      
+      //swal("Numero no registrado" , "tienes que registrarte", "error");
+    
+    
   }
 
   vercodigo(value: string) {
@@ -80,10 +103,11 @@ export class SMSMComponent {
             firebase.auth().currentUser?.getIdToken().then(
               token => {
                 swal("Genial Bienvenido" , "", "success");
+                this.sms.tipo_us=1;
                 this.token = token;
                 console.log(this.token)
                 this.sms.iniciartoken(this.token);
-                this.router.navigate(['citas-reservadas']);
+                this.router.navigate(['']);
               }
             )
           }
